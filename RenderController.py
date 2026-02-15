93588855f8d7ac4c8bc62c29fd4ebd1ef2bbca9a
Tim <enguji123@gmail.com>
2026-02-15 15:05:05 -0700

    Fleshed out render

--- RenderController.py
+++ RenderController.py
@@ -2,8 +2,9 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 class RenderController():
-    def con_matrix_to_edges(self, cm, labels):
+    def con_matrix_to_edges_and_color_map(self, cm, labels, sia):
         edges = []
+        edge_color_map = []
 
         for i in range(len(cm)):
             from_node = labels[i]
@@ -11,28 +12,48 @@ class RenderController():
             for j in range(len(cm[i])):
                 to_node = labels[j]
 
-                if cm[i][j] == 1:
+                if cm[i][j] != 0:
                     edges.append((from_node, to_node))
 
-        return edges
+                    if i in sia.cut.from_nodes and j in sia.cut.to_nodes:
+                        edge_color_map.append('tomato')
+                    else:
+                        edge_color_map.append('lightgrey')
+
+        return edges, edge_color_map
+
+    def nodes_to_color_map(self, nodes, sia):
+        color_map = []
+
+        for node in nodes:
+            if sia.subsystem.node_labels.index(node) in sia.subsystem.node_indices:
+                color_map.append('tomato')
+
+            else:
+                color_map.append('lightblue')
+                print(node, sia.subsystem.node_indices)
+
+
+        return color_map
 
     def __init__(self, network, sia):
         self.graph = nx.DiGraph()
 
         nodes = network.node_labels
-        edges = self.con_matrix_to_edges(network.cm, nodes)
+        node_color_map = self.nodes_to_color_map(nodes, sia)
+        edges, edge_color_map = self.con_matrix_to_edges_and_color_map(network.cm, nodes, sia)
         self.graph.add_nodes_from(nodes)
         self.graph.add_edges_from(edges)
 
-        plt.figure(figsize=(8, 6))
+        plt.figure(figsize=(10, 7))
         ax = plt.gca()
 
         ax.set_title('System Graph')
-        plt.text(0.02, 0.98, f'Phi: {sia.phi} bits', transform=ax.transAxes, verticalalignment='top')
-        plt.text(0.02, 0.95, f'Major Complex: {sia.subsystem}', transform=ax.transAxes, verticalalignment='top')
-
+        plt.text(-0.04, 1.09, f'Phi: {sia.phi} bits', color='tomato', transform=ax.transAxes, verticalalignment='top')
+        plt.text(-0.04, 1.06, f'Major Complex: {sia.subsystem}', color='tomato', transform=ax.transAxes, verticalalignment='top')
+        plt.text(-0.04, 1.03, f'MIP: {sia.cut}', color='orange', transform=ax.transAxes, verticalalignment='top')
 
         pos = nx.spring_layout(self.graph)
 
-        nx.draw(self.graph, pos, ax=ax, with_labels=True, node_color='lightblue', node_size=2000, font_weight='bold', arrows=True)
+        nx.draw(self.graph, pos, ax=ax, with_labels=True, node_color=node_color_map, edge_color=edge_color_map, node_size=2000, font_weight='bold', arrows=True)
         plt.show()
--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import collections
 import ModelInterpreter
 import RenderController
 
-# This fixes the issue for ALL pyphi files at once
 collections.Iterable = collections.abc.Iterable
 collections.Mapping = collections.abc.Mapping
 collections.Sequence = collections.abc.Sequence
@@ -26,31 +25,23 @@ if __name__ == '__main__':
         'C:/Users/Tim/Desktop/Computer_Vision/Computer-Vision/Version_Results/Model_2/Models/model_epoch5.pth')
 
     tpm = np.array([
-        [0, 0, 0, 0], [0, 0, 1, 0], [1, 0, 1, 0], [1, 0, 0, 1],  # States 0-3
-        [1, 0.6, 0, 0], [1, 1, 1, 0], [1, 1, 1, 1], [1, 1, 0, 1],  # States 4-7
-        [0, 1, 1, 1], [0, 1, 0, 0], [1, 0, 1, 1], [1, 1, 0, 0],  # States 8-11
-        [1, 0, 0, 0], [0, 0, 1, 1], [1, 1, 1, 0], [1, 0, 1, 0]  # States 12-15
+        [0, 0, 0, 0], [0, 0, 1, 0], [1, 0, 1, 0], [1, 0, 0, 1],
+        [1, 0.6, 0, 0], [1, 1, 1, 0], [1, 1, 1, 1], [1, 1, 0, 1],
+        [0, 1, 1, 1], [0, 1, 0, 0], [1, 0, 1, 1], [1, 1, 0, 0],
+        [1, 0, 0, 0], [0, 0, 1, 1], [1, 1, 1, 0], [1, 0, 1, 0]
     ])
 
     cm = np.array([
-        [0, 1, 1, 0],  # A influences B, C
-        [1, 0, 1, 0],  # B influences A, C
-        [1, 1, 0, 1],  # C influences A, B, D
-        [0, 0, 1, 1]  # D influences C
+        [0, 1, 1, 0],
+        [1, 0, 1, 0],
+        [1, 1, 0, 1],
+        [0, 0, 1, 1]
     ])
 
     labels = ('A', 'B', 'C', 'D')
-    state = (1, 0, 1, 1)
 
     network = pyphi.Network(tpm, cm=cm, node_labels=labels)
-
-
+    state = (1, 0, 1, 1)
     sys_irreducibility_analysis = pyphi.compute.major_complex(network, state)
-    print(sys_irreducibility_analysis)
-    print(sys_irreducibility_analysis.subsystem)
-
 
     render_controller = RenderController.RenderController(network, sys_irreducibility_analysis)
-
-
-
