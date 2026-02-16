793742333f9a35831a94d866498f8a97f6759813
Tim <enguji123@gmail.com>
2026-02-15 17:12:35 -0700

    refactored for datagen

--- main.py
+++ main.py
@@ -1,47 +1,34 @@
 import os
+import warnings
+from numpy.exceptions import VisibleDeprecationWarning
 os.environ['PYPHI_WELCOME_OFF'] = 'yes'
+warnings.filterwarnings("ignore", category=VisibleDeprecationWarning)
+
 import collections.abc
 import collections
-
-import ModelInterpreter
-import RenderController
-
 collections.Iterable = collections.abc.Iterable
 collections.Mapping = collections.abc.Mapping
 collections.Sequence = collections.abc.Sequence
 collections.MutableMapping = collections.abc.MutableMapping
 
+from ModelInterpreter import ModelInterpreter
+from RenderController import RenderController
+from DatasetGenerator import DatasetGenerator
+
 import pyphi
 import numpy as np
 
 pyphi.config.PROGRESS_BARS = True
-pyphi.config.VALIDATE_SUBSYSTEM_STATES = True
+pyphi.config.VALIDATE_SUBSYSTEM_STATES = False
 pyphi.config.PARALLEL_CONCEPT_EVALUATION = False
-pyphi.config.PARALLEL_CUT_EVALUATION = False
+pyphi.config.PARALLEL_CUT_EVALUATION = True
 pyphi.config.PARALLEL_COMPLEX_EVALUATION = False
 
 if __name__ == '__main__':
-    model_interpreter = ModelInterpreter.ModelInterpreter(
-        'C:/Users/Tim/Desktop/Computer_Vision/Computer-Vision/Version_Results/Model_2/Models/model_epoch5.pth')
+    dataset_generator = DatasetGenerator()
+    #model_interpreter = ModelInterpreter('C:/Users/Tim/Desktop/Computer_Vision/Computer-Vision/Version_Results/Model_2/Models/model_epoch5.pth')
 
-    tpm = np.array([
-        [0, 0, 0, 0], [0, 0, 1, 0], [1, 0, 1, 0], [1, 0, 0, 1],
-        [1, 0.6, 0, 0], [1, 1, 1, 0], [1, 1, 1, 1], [1, 1, 0, 1],
-        [0, 1, 1, 1], [0, 1, 0, 0], [1, 0, 1, 1], [1, 1, 0, 0],
-        [1, 0, 0, 0], [0, 0, 1, 1], [1, 1, 1, 0], [1, 0, 1, 0]
-    ])
-
-    cm = np.array([
-        [0, 1, 1, 0],
-        [1, 0, 1, 0],
-        [1, 1, 0, 1],
-        [0, 0, 1, 1]
-    ])
-
-    labels = ('A', 'B', 'C', 'D')
-
-    network = pyphi.Network(tpm, cm=cm, node_labels=labels)
-    state = (1, 0, 1, 1)
+    network, state = dataset_generator.generate_network_and_state()
     sys_irreducibility_analysis = pyphi.compute.major_complex(network, state)
 
-    render_controller = RenderController.RenderController(network, sys_irreducibility_analysis)
+    render_controller = RenderController(network, sys_irreducibility_analysis)
